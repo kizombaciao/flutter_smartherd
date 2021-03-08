669125a86325b6a61fed9027b09bcf152e7604fc
Kizomba <kizomba@github.com>
2021-03-08 13:09:22 +0100

    1.5

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,28 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
+void main() => runApp(MyFlutterApp());
+
+class MyFlutterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'My Flutter App - MaterialApp',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Scafford'),
+          title: Text('AppBar'),
         ),
         body: Material(
           color: Colors.lightBlueAccent,
           child: Center(
             child: Text(
-              "Hello Flutter - Center",
+              "Hello Flutter - Body",
               textDirection: TextDirection.ltr,
               style: TextStyle(color: Colors.white, fontSize: 40.0),
             ),
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
