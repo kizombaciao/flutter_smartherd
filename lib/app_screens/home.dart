39799b10c6eb74b5708414985c347eb630377c10
Kizomba <kizomba@github.com>
2021-03-08 18:08:55 +0100

    2.3

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -12,7 +12,17 @@ class Home extends StatelessWidget {
         //margin: EdgeInsets.only(left: 35.0, top: 50.0),
         //padding: EdgeInsets.all(15.0),
 
-        child: Text('Flight', textDirection: TextDirection.ltr),
+        child: Text(
+          'Flight',
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+            decoration: TextDecoration.none,
+            fontSize: 75.0,
+            fontFamily: 'Raleway',
+            fontWeight: FontWeight.w700,
+            color: Colors.white,
+          ),
+        ),
       ),
     );
   }
