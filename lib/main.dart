ac8a72daed5775d0584e35f4f5aedf8932d16daf
Kizomba <kizomba@github.com>
2021-03-08 12:50:23 +0100

    1.4

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,22 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    Center(
-      child: Text(
-        "Hello Flutter",
-        textDirection: TextDirection.ltr,
+    MaterialApp(
+      title: 'My Flutter App - MaterialApp',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Scafford'),
+        ),
+        body: Material(
+          color: Colors.lightBlueAccent,
+          child: Center(
+            child: Text(
+              "Hello Flutter - Center",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(color: Colors.white, fontSize: 40.0),
+            ),
+          ),
+        ),
       ),
     ),
   );
